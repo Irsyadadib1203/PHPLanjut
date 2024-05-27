c81b33bcaefc779997d506ef6d207bc464438e01
irsyadadib1203 <irsyadadib1234@gmail.com>
2024-05-27 10:22:15 +0700

    PHP Lanjut

---
+++ 2_ProsesPendaftaran.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+<body>
+Selamat datang <b><?php echo $_POST["nama"]; ?></b><br>
+NIM: <?php echo $_POST["nim"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Tempat, tanggal Lahir: <?php echo $_POST["tempat_lahir"]; ?>, <?php echo $_POST["tanggal_lahir"]; ?><br>
+Alamat: <?php echo $_POST["alamat"]; ?><br>
+Jenis Kelamin <?php echo $_POST["jenis_kelamin"]; ?><br>
+<body>
+</html>
